c8cd8dd77e35072b98fca36dc9845a27cd2026d1
Dragan Balatinac <balatinac@cp-compartner.de>
2023-11-08 13:34:59 +0100

    Add app folder creation and .env symlink in preDeploy hook

--- packages/deployment/hooks/preDeploy.js
+++ packages/deployment/hooks/preDeploy.js
@@ -1,6 +1,10 @@
 const hookUtility = require("./hookUtility");
 
 const commands = [
+    // Check if app folders exist and create them if not
+    "[ -d apps/backend ] || mkdir -p apps/backend",
+    "[ -d apps/frontend ] || mkdir -p apps/frontend",
+
     // Creates a .env if not present in source folder
     "[ -h .env ] || ln -s ../shared/.env .env",
     "[ -h apps/backend/.env ] || ln -s ../../.env apps/backend/.env",
