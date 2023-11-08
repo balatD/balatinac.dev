5bc03ff68b9e0af8507a70b91bcf053f497de61e
Dragan Balatinac <balatinac@cp-compartner.de>
2023-11-08 17:16:58 +0100

    Update pm2 configuration path.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,5 +1,5 @@
 // For additional settings look into the packages/deployment folder
-const pm2Configuration = require('balatinac-dev-deployment/pm2-config');
+const pm2Configuration = require('./packages/deployment/pm2-config');
 
 module.exports = {
     ...pm2Configuration
