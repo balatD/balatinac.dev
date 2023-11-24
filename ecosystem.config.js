319e4ac0f84187b2c427a929cfc76404a4bf9ee9
Dragan Balatinac <balatinac@cp-compartner.de>
2023-11-24 21:46:58 +0100

    add dokku monorepo plugin

--- ecosystem.config.js
+++
@@ -1,6 +0,0 @@
-// For additional settings look into the packages/deployment folder
-const pm2Configuration = require('./packages/deployment/pm2-config');
-
-module.exports = {
-    ...pm2Configuration
-}
